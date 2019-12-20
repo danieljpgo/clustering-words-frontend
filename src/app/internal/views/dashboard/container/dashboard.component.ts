91046e221702f8b25a57622e4d2380e4a74e69eb
Daniel Jorge <djpgonzaga@gmail.com>
2019-12-20 08:24:39 -0300

    finish layout, select actions, list options

--- src/app/internal/views/dashboard/container/dashboard.component.ts
+++ src/app/internal/views/dashboard/container/dashboard.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {DashboardService} from '../services/dashboard.service';
 import {RequestParams} from '../../../../../utils/models/http.interface';
+import {animations} from '../../../../../utils/animations';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: animations
 })
 export class DashboardComponent implements OnInit {
 
@@ -19,14 +21,19 @@ export class DashboardComponent implements OnInit {
 
   // Form
   vocabularyForm: FormGroup;
+  vocabularyGenForm: FormGroup;
 
-  response;
+  vocabularies = [];
+  selectId;
+  respost;
 
   constructor(private formBuilder: FormBuilder, private dashboardService: DashboardService) {
     this.vocabularyForm = this.formBuilder.group({
       text: [null,
         Validators.required
-      ],
+      ]
+    });
+    this.vocabularyGenForm = this.formBuilder.group({
       options: [0,
         Validators.required
       ]
@@ -34,24 +41,54 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.dashboardService.listVocabularies(this.requestParams)
-      .subscribe((vocabularies) => {
-        console.log(vocabularies);
-      });
+    this.handleListVocab();
   }
 
-  handleSubmit(): void {
+  handleSubmitText(): void {
     const data = { text: this.vocabularyForm.get('text').value };
-
-    console.log(this.dashboardService);
-
     this.dashboardService.createVocabularies(data)
-      .subscribe((test) => {
-        console.log(test);
+      .subscribe(() => {
+        this.handleListVocab();
       }, error => {
         console.log(error);
       });
-    // console.log(this.vocabularyForm.get('text').value);
+  }
+
+
+  handleSelect(vocabulary: any): void {
+    this.selectId = vocabulary.id;
+  }
+
+  handleSelectGen(id: string): void {
+    switch (this.vocabularyGenForm.get('options').value) {
+      case 0: this.dashboardService.getVocabIsolated(id)
+        .subscribe((value) => {
+          this.respost = value;
+        });
+              break;
+      case 1: this.dashboardService.getVocabGroup(id)
+        .subscribe((value) => {
+          this.respost = value;
+        });
+              break;
+      case 2: this.dashboardService.getVocabIsolatedVector(id)
+        .subscribe((value) => {
+          this.respost = value;
+        });
+              break;
+      case 3: this.dashboardService.getVocabGroupVector(id)
+        .subscribe((value) => {
+          this.respost = value;
+        });
+              break;
+    }
+  }
+
+  handleListVocab(): void {
+    this.dashboardService.listVocabularies(this.requestParams)
+      .subscribe((vocabularies: any[]) => {
+        this.vocabularies = vocabularies;
+      });
   }
 
 }
